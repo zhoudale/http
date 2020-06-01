fc58fb28ec90ef938858115d4f5b4af4d8809577
zhoudale <lez3@uci.edu>
2020-06-01 14:02:31 +0800

    before lunch

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, request, abort
+import click
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/hello')
+@app.route('/hello', methods = ['GET', 'POST'])
 def index():
-    return '<h1>Hello</h1>'
+    return '<h1>Hello</h1>',302,{'Location':'https://www.baidu.com'}
 #
 # @app.route('/greet/<name>')
 # @app.route('/greet')
@@ -17,3 +18,24 @@ def index():
 @app.route('/greet/<name>')
 def say_hello(name):
     return f'<h1>Hello, {name}!</h1>'
+
+
+@app.route('/brew')
+def tea():
+    abort(418)
+
+@app.cli.command()
+def greet():
+    """Say Hi"""
+    click.echo('Hello!')
+
+
+@app.route('/user', methods=['POST'])
+def user():
+    name = request.args.get('name', 'Human')
+    return f'<h1>Hi, {name}</h1>'
+
+
+@app.route('/go_back/<int:year>')
+def to_year(year):
+    return f'<h1>Welcome to year {2020-year}</h1>'
